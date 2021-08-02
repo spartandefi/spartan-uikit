dc505ef66209b7698dd5b42ee8b3daac862211fb
spartandefi <spartandefi@gmail.com>
2021-08-02 14:08:19 +0800

    w

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2730,17 +2730,17 @@ var baseColors = {
     primary: "#708090",
     primaryBright: "#708090",
     primaryDark: "#708090",
-    secondary: "#bb0a1e",
+    secondary: "#ffc42e",
     success: "#31D0AA",
     warning: "#FFB237",
 };
 var brandColors = {
     binance: "#F0B90B",
 };
-var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#a48111", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#bb0a1e", textDisabled: "#BDC2C4", textSubtle: "#bb0a1e", borderColor: "#a48111", card: "#a48111", gradients: {
+var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#8a0303", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#ffc42e", textDisabled: "#BDC2C4", textSubtle: "#ffc42e", borderColor: "#8a0303", card: "#8a0303", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
-var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#bb0a1e", background: "#a48111", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", primaryDark: "#708090", tertiary: "#EFF4F5", text: "#bb0a1e", textDisabled: "#BDC2C4", textSubtle: "#bb0a1e", borderColor: "#a48111", card: "#a48111", gradients: {
+var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#ffc42e", background: "#8a0303", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", primaryDark: "#708090", tertiary: "#EFF4F5", text: "#ffc42e", textDisabled: "#BDC2C4", textSubtle: "#ffc42e", borderColor: "#8a0303", card: "#8a0303", gradients: {
         bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
     } });
 
